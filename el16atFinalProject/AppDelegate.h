a56db836aca8b932185cfcb2089dd5e1b1175aaf
Alice Tiler [el16at] <el16at@campus-10-41-15-107.wireless.leeds.ac.uk>
2017-11-23 18:07:42 +0000

    Initial Commit

---
+++ el16atFinalProject/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  el16atFinalProject
+//
+//  Created by Alice Tiler [el16at] on 23/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
